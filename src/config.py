c606b75c058d5e87cc9fd3154ed6cb3b2e2a5eea
nbirchde <nicholas.birch.de.la.calle@ulb.be>
2025-05-17 00:43:06 +0200

    clean

--- src/config.py
+++ src/config.py
@@ -1,11 +1,3 @@
 #!/usr/bin/env python3
-"""
-Configuration file for the Round Robin project.
-
-Stores global constants, including the chosen weights for the objective function.
-"""
-
-ALPHA = 0.8 # Updated to new chosen value.
-BETA = 1.2  # Updated to new chosen value.
-
-# Other configurations can be added here as needed.
+ALPHA = 0.8
+BETA = 1.2 
